d96e4c6e06cdfc66a3cd732f2eafb4fa78efa0eb
007shishir <islam7.saiful@gmail.com>
2026-02-23 23:09:18 +0600

    toggle button function added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let totalJobCount = document.getElementById("totalJobCount");
 let interviewCount = document.getElementById("interviewCount");
 let rejectedCount = document.getElementById("rejectedCount");
 
+
 function updateCounts() {
     let totalJobs = document.getElementById("allJobCard").children.length;
     totalJobCount.innerHTML = totalJobs;
@@ -13,3 +14,30 @@ function updateCounts() {
 }
 
 updateCounts();
+
+
+
+// Toggle three button filter
+let allJobCardbtn = document.getElementById("allJobCardbtn");
+let interviewJobCardbtn = document.getElementById("interviewJobCardbtn");
+let rejectedJobCardbtn = document.getElementById("rejectedJobCardbtn");
+
+function toggleJobCards(id) {
+    allJobCardbtn.classList.remove("btn-primary");
+    interviewJobCardbtn.classList.remove("btn-primary");
+    rejectedJobCardbtn.classList.remove("btn-primary");
+
+    if (id === "allJobCardbtn") {
+        allJobCardbtn.classList.add("btn-active");
+        interviewJobCardbtn.classList.add("btn-primary");
+        rejectedJobCardbtn.classList.add("btn-primary");
+    } else if (id === "interviewJobCardbtn") {
+        interviewJobCardbtn.classList.add("btn-active");
+        allJobCardbtn.classList.add("btn-primary");
+        rejectedJobCardbtn.classList.add("btn-primary");
+    } else if (id === "rejectedJobCardbtn") {
+        rejectedJobCardbtn.classList.add("btn-active");
+        interviewJobCardbtn.classList.add("btn-primary");
+        allJobCardbtn.classList.add("btn-primary");
+    }
+}
