4cbc0187581f7a7f12be10c7b9851550e37b47d3
007shishir <islam7.saiful@gmail.com>
2026-02-24 14:46:43 +0600

    btn and counting working

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let interviewCountArray = [];
 let rejectedCountArray = [];
 
+const mainContainer = document.getElementById("mainContainer");
+
 let totalJobCount = document.getElementById("totalJobCount");
 let interviewCount = document.getElementById("interviewCount");
 let rejectedCount = document.getElementById("rejectedCount");
 
+const filterSection = document.getElementById("filterSection");
+
+const allJobCard = document.getElementById("allJobCard");
+
 
 function updateCounts() {
-    let totalJobs = document.getElementById("allJobCard").children.length;
+    let totalJobs = document.getElementById("cardHolder").children.length;
     totalJobCount.innerHTML = totalJobs;
 
     interviewCount.innerHTML = interviewCountArray.length;
     rejectedCount.innerHTML = rejectedCountArray.length;
-    console.log(interviewCountArray.length);
 
 }
 
@@ -34,22 +39,32 @@ function toggleJobCards(id) {
         allJobCardbtn.classList.add("btn-active");
         interviewJobCardbtn.classList.add("btn-primary");
         rejectedJobCardbtn.classList.add("btn-primary");
+        allJobCard.classList.remove("hidden");
+        filterSection.classList.add("hidden");
+        allJobCard.classList.add("visible");
     } else if (id === "interviewJobCardbtn") {
         interviewJobCardbtn.classList.add("btn-active");
         allJobCardbtn.classList.add("btn-primary");
         rejectedJobCardbtn.classList.add("btn-primary");
+        allJobCard.classList.add("hidden");
+        filterSection.classList.remove("hidden");
+        filterSection.classList.add("visible");
+        renderFilteredCards("interview");
     } else if (id === "rejectedJobCardbtn") {
         rejectedJobCardbtn.classList.add("btn-active");
         interviewJobCardbtn.classList.add("btn-primary");
         allJobCardbtn.classList.add("btn-primary");
+        allJobCard.classList.add("hidden");
+        filterSection.classList.remove("hidden");
+        filterSection.classList.add("visible");
+        renderFilteredCards("rejected");
     }
 }
 
 
-document.getElementById("allJobCard").addEventListener("click", function (event) {
+document.getElementById("mainContainer").addEventListener("click", function (event) {
     const parentNode = event.target.parentNode.parentNode;
 
-    console.log(parentNode);
 
     if (event.target.classList.contains("interviewBtnBadge")) {
 
@@ -58,10 +73,10 @@ document.getElementById("allJobCard").addEventListener("click", function (event)
         const jobRequirement = parentNode.querySelector(".job-requirement").innerText;
         const jobDescription = parentNode.querySelector(".job-description").innerText;
         let status = parentNode.querySelector(".badge-status").innerText;
-        
+
         status = "INTERVIEW";
         parentNode.querySelector(".badge-status").innerText = status;
-        
+
         const filteredCard = {
             cardTitle,
             jobDetails,
@@ -119,21 +134,51 @@ document.getElementById("allJobCard").addEventListener("click", function (event)
             updateCounts();
         }
     }
-
-
-    // if (parentNode.classList.contains("interviewBtnBadge")) {
-    //     const div = document.createElement("div");
-    //     div.classList.add("card-holder", "space-y-5");
-    //     div.innerHTML = `
-    //     `;
-    //     document.querySelector(".filterSection").appendChild(div);          
-    // } else if (parentNode.classList.contains("not-applied")) {
-
-    // }
 });
 
-function renderFilteredCards() {
+function renderFilteredCards(filterType) {
 
+    filterSection.innerHTML = "";
+
+    if (filterType === 'interview') {
+        interviewCountArray.forEach(card => {
+
+            const div = document.createElement("div");
+            div.classList.add("card", "bg-base-100", "shadow-sm");
+            div.innerHTML = `
+            <div class="card-body">
+                <h2 class="card-title">${card.cardTitle}</h2>
+                <p class="job-details">${card.jobDetails}</p>
+                <p class="job-requirement">${card.jobRequirement}</p>
+                <p class="job-description">${card.jobDescription}</p>
+                <div class="badge-status badge badge-primary badge-outline">${card.status}</div>
+                <div class="card-actions mt-5">
+                <button class="interviewBtnBadge badge badge-outline text-success">Interview</button>
+                <button class="rejectedBtnBadge badge badge-outline text-error">Rejected</button>
+              </div>
+            </div>
+        `;
+            filterSection.appendChild(div);
+        });
+    } else if (filterType === 'rejected') {
+        rejectedCountArray.forEach(card => {
+            const div = document.createElement("div");
+            div.classList.add("card", "bg-base-100", "shadow-sm");
+            div.innerHTML = `
+            <div class="card-body">
+                <h2 class="card-title">${card.cardTitle}</h2>
+                <p class="job-details">${card.jobDetails}</p>
+                <p class="job-requirement">${card.jobRequirement}</p>
+                <p class="job-description">${card.jobDescription}</p>
+                <div class="badge-status badge badge-primary badge-outline">${card.status}</div>
+                                    <div class="card-actions mt-5">
+                        <button class="interviewBtnBadge badge badge-outline text-success">Interview</button>
+                        <button class="rejectedBtnBadge badge badge-outline text-error">Rejected</button>
+                    </div>
+            </div>
+        `;
+            filterSection.appendChild(div);
+        });
+    }
 }
-
 updateCounts();
